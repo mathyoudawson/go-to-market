fcb00e9e7b84983bb90c5bfcd704bc8b0828d9cf
Matthew Dawson <matthewdawson@outlook.com>
2019-07-01 16:51:47 +1200

    Enable users to log in with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.extra.id_info.email
+      user.token = auth.credentials.token
+      user.expires = auth.credentials.experies
+      user.expires_at = auth.credentials.expires_at
+      user.refresh_token = auth.credentials.refresh_token
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, :skip => [:registrations] , :controllers => {
+    :omniauth_callbacks => "users/omniauth_callbacks",
+  }
+
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  root 'static_pages#home'
 end
